31cc598a65bcbe23ebfc4071bdc9b0f8acaf48ca
MussahaB <mussahabf8@gmail.com>
2026-03-19 00:23:57 +0530

    Add flake8 config

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Task',
+            name="Task",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('description', models.TextField(blank=True)),
-                ('status', models.CharField(default='pending', max_length=50)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("description", models.TextField(blank=True)),
+                ("status", models.CharField(default="pending", max_length=50)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]

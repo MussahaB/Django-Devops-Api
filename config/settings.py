92c92c3d160904fe7f3f050384b80b7ae50712c4
MussahaB <mussahabf8@gmail.com>
2026-04-01 20:59:37 +0530

    Fix CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
-CSRF_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
     "https://django-devops-api.onrender.com"
-]
+).split(",")
 
 # Application definition
 

f54a6f61e6173546ccd5855d15c1cea96a82843e
Mussahab <febinfrenzy@gmail.com>
2026-05-12 20:05:23 +0000

    Fix environment-based database settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,23 +105,23 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('POSTGRES_DB'),
-    #     'USER': os.environ.get('POSTGRES_USER'),
-    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-    #     'HOST': os.environ.get('POSTGRES_HOST'),
-    #     'PORT': os.environ.get('POSTGRES_PORT'),
-    # }
-    'default': dj_database_url.config(
-        default=(
-            f"postgres://{os.environ.get('POSTGRES_USER')}:"
-            f"{os.environ.get('POSTGRES_PASSWORD')}@"
-            f"{os.environ.get('POSTGRES_HOST')}:"
-            f"{os.environ.get('POSTGRES_PORT')}/"
-            f"{os.environ.get('POSTGRES_DB')}"
-        )
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+    # 'default': dj_database_url.config(
+    #     default=(
+    #         f"postgres://{os.environ.get('POSTGRES_USER')}:"
+    #         f"{os.environ.get('POSTGRES_PASSWORD')}@"
+    #         f"{os.environ.get('POSTGRES_HOST')}:"
+    #         f"{os.environ.get('POSTGRES_PORT')}/"
+    #         f"{os.environ.get('POSTGRES_DB')}"
+    #     )
+    # )
 }
 
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,7 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
+DATABASES['default'] = dj_database_url.config(
+    default=os.getenv('DATABASE_URL')
+)
